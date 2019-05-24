263968c65374ce460e1dbb4d191d079ed361bbeb
Sven Knobloch <sven98@me.com>
2019-05-24 16:49:25 +0200

    Fix run script

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ PID_POST_STATS=$!
 cargo run --release --bin dspa-recommendations -- --users 0 1 2 3 4 5 6 7 8 9 > recommendations.log &
 PID_RECOMMENDATIONS=$!
 
-cargo run --release --bin dspa-anomalies --threshold=0 > anomalies.log &
+cargo run --release --bin dspa-anomalies -- --threshold=0 > anomalies.log &
 PID_ANOMALIES=$!
 
 cargo run --release --bin dspa-source -- --streams ./data/1k-users-sorted/ --speedup=3600 --delay=3600
